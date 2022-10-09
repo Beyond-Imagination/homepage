82345c6d7709c21366d6ebf38e8b29356151857d
YunChang <ycc4477@gmail.com>
2022-10-09 10:33:40 +0900

    feat: project detail page 작성 (#2)

---
+++ components/project/detail/ProjectDetailPhoto.jsx
@@ -0,0 +1,95 @@
+const ProjectDetailPhoto = ({ project, photoNum, selectPhotoNum }) => {
+  return (
+    <div id="default-carousel" className="relative">
+      <div className="relative h-[720px] overflow-hidden rounded-lg">
+        {project.photos.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className={`duration-700 ease-in-out ${
+                index === photoNum ? '' : 'hidden'
+              }`}
+            >
+              <img
+                src={value}
+                className="absolute block h-[720px] object-fill -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="absolute z-50 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+        {project.photos.map((value, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`w-3 h-3 rounded-full ${
+                index === photoNum ? 'bg-amber-50' : 'bg-[#353F4D]'
+              }`}
+              onClick={() => {
+                selectPhotoNum(index)
+              }}
+            ></button>
+          )
+        })}
+      </div>
+      <button
+        type="button"
+        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        onClick={() => {
+          selectPhotoNum(Math.max(photoNum - 1, 0))
+        }}
+      >
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-400/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 19l-7-7 7-7"
+            ></path>
+          </svg>
+          <span className="sr-only">Previous</span>
+        </span>
+      </button>
+      <button
+        type="button"
+        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        onClick={() => {
+          selectPhotoNum(Math.min(photoNum + 1, project.photos.length - 1))
+        }}
+      >
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-400/30 group-hover:bg-white/50 dark:group-hover:bg-gray-400/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5l7 7-7 7"
+            ></path>
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default ProjectDetailPhoto
--- pages/project/[id]/index.jsx
+++ pages/project/[id]/index.jsx
@@ -1,10 +1,164 @@
 import { useRouter } from 'next/router'
+import { contentfulClientApi } from '@/utils/contentfu-api'
+import ProjectCardList from '@/components/project/CardList.project'
+import { useState } from 'react'
+import photo from '../../photo'
+import ProjectDetailPhoto from '@/components/project/detail/ProjectDetailPhoto'
+import Link from 'next/link'
+
+const ProjectDetailContent = (props) => {
+  return (
+    <div className={`mb-5`}>
+      <div>
+        <h2 className={`text-3xl font-bold mb-2`}>{props.title}</h2>
+      </div>
+      <div className={`py-4`}>
+        <div className={`text-xl`}>{props.content}</div>
+      </div>
+    </div>
+  )
+}
 
 function ProjectDetail(props) {
+  const { entry } = props
   const router = useRouter()
-  const { id } = router.query
+  const [photoNum, selectPhotoNum] = useState(0)
 
-  return <div>Project Detail id: {id}</div>
+  const project = entry.items[0].fields
+  console.log(project)
+  return (
+    <div className={`h-full`}>
+      <div className={`flex justify-center`}>
+        <h1 className={`text-5xl font-bold my-12`}>
+          {`${project.name} 프로젝트`}
+        </h1>
+      </div>
+      <div></div>
+      <ProjectDetailPhoto
+        project={project}
+        photoNum={photoNum}
+        selectPhotoNum={selectPhotoNum}
+      ></ProjectDetailPhoto>
+      <div className={`flex gap-4 justify-end`}>
+        {project.github?.map((git, index) => {
+          return (
+            <Link key={git.key} href={git.value}>
+              <a target={`_blank`}>
+                <div className={`flex items-center cursor-pointer`}>
+                  <span className={`material-icons mr-1`}>link</span>
+                  <span>{`${git.key} Github`}</span>
+                </div>
+              </a>
+            </Link>
+          )
+        })}
+        {project.youtube?.map((link, index) => {
+          return (
+            <Link key={index} href={link}>
+              <a target={`_blank`}>
+                <div className={`flex items-center cursor-pointer`}>
+                  <span className={`material-icons mr-1`}>link</span>
+                  <span>Youtube Link</span>
+                </div>
+              </a>
+            </Link>
+          )
+        })}
+      </div>
+      <ProjectDetailContent
+        title={'프로젝트명'}
+        content={project.name}
+      ></ProjectDetailContent>
+      <ProjectDetailContent
+        title={'설명'}
+        content={project.description.content?.map((v, index) => (
+          <div key={index}>{v.content[0].value}</div>
+        ))}
+      ></ProjectDetailContent>
+      <ProjectDetailContent
+        title={'참여멤버'}
+        content={project.members?.map((v, index) => (
+          <span key={index} className={`mr-2`}>{`${v}`}</span>
+        ))}
+      ></ProjectDetailContent>
+
+      <ProjectDetailContent
+        title={'프로젝트 기간'}
+        content={`${project.start_at} ~ ${project.end_at}`}
+      ></ProjectDetailContent>
+      <ProjectDetailContent
+        title={'기술 스택'}
+        content={project.tech_stacks?.map((v, index) => (
+          <span key={index} className={`mr-2`}>{`${v}`}</span>
+        ))}
+      ></ProjectDetailContent>
+      <ProjectDetailContent
+        title={'수상 경력'}
+        content={
+          project.prizes.length > 0 ? (
+            project.prizes?.map((v, index) => (
+              <div key={index}>{`${v.prize}, ${v.date} (${v.prize})  `}</div>
+            ))
+          ) : (
+            <div>{`${project.prizes.value}`}</div>
+          )
+        }
+      ></ProjectDetailContent>
+      <ProjectDetailContent
+        title={'첨부 파일'}
+        content={project.attachments?.map((v, index) => (
+          <div key={index}>
+            <Link href={v.fields.file.url}>
+              <a
+                href={v.fields.file.url}
+                target="_blank"
+                rel="noreferrer"
+                className={`hover:underline`}
+              >
+                {v.fields.file.fileName}
+              </a>
+            </Link>
+          </div>
+        ))}
+      ></ProjectDetailContent>
+    </div>
+  )
 }
 
 export default ProjectDetail
+//Server Side에서 API 요청을 위한 함수
+export async function getServerSideProps(context) {
+  const { req, res } = context
+  const { id } = context.query
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  )
+  const entries = await contentfulClientApi.getEntries({
+    select: 'fields',
+    content_type: 'projects',
+    // order: 'fields.join_date',
+    'sys.id': id,
+  })
+
+  const map = new Map()
+  entries.includes.Asset.forEach((asset) => {
+    const key = asset.sys.id
+    const value = `https:${asset.fields.file.url}`
+    map.set(key, value)
+  })
+  entries.items.forEach((item) => {
+    let photos = []
+    item.fields.photos.forEach((photo) => {
+      photos.push(map.get(photo.sys.id))
+    })
+    item.fields.photos = photos
+  })
+
+  return {
+    props: {
+      entry: entries,
+    }, // will be passed to the page component as props
+  }
+}
