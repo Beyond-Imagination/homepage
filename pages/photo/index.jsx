3f6a2e47493e4186ce494bcafad192c09595d496
Kyung <xoelxowl92@gmail.com>
2023-03-28 15:19:46 +0000

    photo album serverSide rendering -> clientSide rendering
    Merge-request: B-I-HOMEPAGE-MR-3
    Merged-by: 경현 이 <xoelxowl92@gmail.com>

--- pages/photo/index.jsx
+++ pages/photo/index.jsx
@@ -1,6 +1,6 @@
 import { contentfulClientApi } from '@/utils/contentfu-api'
 import { Gallery } from 'react-grid-gallery'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Lightbox from 'yet-another-react-lightbox'
 import 'yet-another-react-lightbox/styles.css'
 import Captions from 'yet-another-react-lightbox/plugins/captions'
@@ -8,11 +8,44 @@ import Zoom from 'yet-another-react-lightbox/plugins/zoom'
 
 import 'yet-another-react-lightbox/plugins/captions.css'
 
-function Photo({ images, entries }) {
+function Photo() {
   const [index, setIndex] = useState(-1)
-  console.log(entries)
-
   const handleClick = (index, item) => setIndex(index)
+
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const entries = await contentfulClientApi.getEntries({
+        select: 'fields',
+        content_type: 'photos',
+        order: '-fields.date',
+      });
+
+      const map = new Map();
+      entries.includes.Asset.forEach((asset) => {
+        const key = asset.sys.id;
+        const value = `https:${asset.fields.file.url}`;
+        map.set(key, value);
+      });
+      entries.items.forEach((item) => {
+        item.fields.photo = map.get(item.fields.photo.sys.id);
+      });
+
+      const images = entries.items.map((v) => {
+        return {
+          src: v.fields.photo,
+          width: 300,
+          height: 300,
+          caption: v.fields.description,
+        };
+      });
+
+      setImages(images);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div>
@@ -35,39 +68,3 @@ function Photo({ images, entries }) {
 }
 export default Photo
 //Server Side에서 API 요청을 위한 함수
-export async function getServerSideProps(context) {
-  const { req, res } = context
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
-  const entries = await contentfulClientApi.getEntries({
-    select: 'fields',
-    content_type: 'photos',
-    order: '-fields.date',
-  })
-
-  const map = new Map()
-  entries.includes.Asset.forEach((asset) => {
-    const key = asset.sys.id
-    const value = `https:${asset.fields.file.url}`
-    map.set(key, value)
-  })
-  entries.items.forEach((item) => {
-    item.fields.photo = map.get(item.fields.photo.sys.id)
-  })
-
-  return {
-    props: {
-      images: entries.items.map((v) => {
-        return {
-          src: v.fields.photo,
-          width: 300,
-          height: 300,
-          caption: v.fields.description,
-        }
-      }),
-      entries,
-    }, // will be passed to the page component as props
-  }
-}
