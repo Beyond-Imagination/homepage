5b2101d7554f0702e7e9187f57b8713bcb8ec8e9
인호 백 <inhobaek.dan@gmail.com>
2023-05-17 16:00:20 +0000

    feat: 배포 스크립트 작성
    - 환경설정 파일 등록
    - yarn build

--- .space.kts
+++ .space.kts
@@ -4,6 +4,16 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build") {
+    container(displayName = "builder", image = "beyond-imagination.registry.jetbrains.space/p/b-i-homepage/containers/builder:latest") {
+		shellScript {
+			content = """
+				echo "NEXT_PUBLIC_SPACE_ID=$NEXT_PUBLIC_SPACE_ID" >> .env
+                echo "NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN=$NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN" >> .env
+                echo "NEXT_PUBLIC_PREVIEW_ACCESS_TOKEN=$NEXT_PUBLIC_PREVIEW_ACCESS_TOKEN" >> .env
+				yarn
+    			yarn build
+      		"""
+        }
+    }
 }
