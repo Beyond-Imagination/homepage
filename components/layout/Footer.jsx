76395ea2f6fe2f0c2be8269b76d809be3c5f9536
Heesoo Jun <acesk123@gmail.com>
2023-06-30 16:04:50 +0000

    footer contact component img to css
    Merge-request: B-I-HOMEPAGE-MR-25
    Merged-by: 희수 전 <acesk123@gmail.com>

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import styles from '../../styles/layout.module.css'
 
 const Footer = ({ className }) => {
   return (
@@ -11,34 +12,44 @@ const Footer = ({ className }) => {
             </a>
           </div>
           <div className="w-1/8 px-12">
-            <h2 className="text-[#5A5A5A] text-lg font-semibold mb-5">
+            <h2
+              className={`text-[#5A5A5A] text-lg font-semibold mb-5 ${styles.FooterFont}`}
+            >
               Category
             </h2>
             <nav className="list-none mb-10 space-y-2">
               <li>
                 <Link href="/member">
-                  <a className="text-[#B4B4B4] text-sm font-light hover:text-gray-800">
+                  <a
+                    className={`${styles.FooterFont} text-[#B4B4B4] text-sm font-light hover:text-gray-800 `}
+                  >
                     Member
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/projects">
-                  <a className="text-[#B4B4B4] text-sm font-light hover:text-gray-800">
+                  <a
+                    className={`${styles.FooterFont} text-[#B4B4B4] text-sm font-light hover:text-gray-800 `}
+                  >
                     Projects
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/history">
-                  <a className="text-[#B4B4B4] text-sm font-light hover:text-gray-800">
+                  <a
+                    className={`${styles.FooterFont} text-[#B4B4B4] text-sm font-light hover:text-gray-800 `}
+                  >
                     History
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/photo">
-                  <a className="text-[#B4B4B4] text-sm font-light hover:text-gray-800">
+                  <a
+                    className={`${styles.FooterFont} text-[#B4B4B4] text-sm font-light hover:text-gray-800 `}
+                  >
                     Photo
                   </a>
                 </Link>
@@ -47,11 +58,41 @@ const Footer = ({ className }) => {
           </div>
           <div className="ml-auto">
             <Link href="mailto:team@beyond-imagination.kr">
-              <a
-                href="mailto:team@beyond-imagination.kr"
-                className="flex items-center"
-              >
-                <img src="/images/ConnectBI.png" alt="Logo" />
+              <a href="mailto:team@beyond-imagination.kr">
+                <div
+                  style={{
+                    position: 'relative',
+                    width: '369px',
+                    height: '140px',
+                    border: '1px solid #B4B4B4',
+                    color: '#B4B4B4',
+                    backgroundColor: '#111111',
+                  }}
+                >
+                  <img
+                    className="mr-3 mt-3"
+                    src="/images/Arrow 1.png"
+                    alt="Logo"
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                    }}
+                  />
+
+                  <div
+                    className={`mb-3 ml-3 text-lg ${styles.FooterFont}`}
+                    style={{
+                      position: 'absolute',
+                      bottom: 0,
+                      left: 0,
+                    }}
+                  >
+                    CONTACT
+                    <br />
+                    Beyond Imagination
+                  </div>
+                </div>
               </a>
             </Link>
           </div>
