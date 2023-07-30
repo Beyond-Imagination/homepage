074b1d1a4410a0515d0dede87e85cc3f7fe32e0c
laggu91 <laggu91@gmail.com>
2023-07-30 14:30:22 +0000

    feat: export project detail page to ssg
    Merge-request: B-I-HOMEPAGE-MR-31
    Merged-by: 구원 라 <laggu91@gmail.com>

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
       '/': { page: '/' },
       '/member': { page: '/member' },
       '/projects': { page: '/projects' },
+      '/projects/1KYfO74OsnYhtvYu3eGxMW': { page: '/projects/[id]' },
+      '/projects/47NFeqbbG1Um9QA16NKCGf': { page: '/projects/[id]' },
+      '/projects/1GjM4jl6OYYfXNKxLnpZMz': { page: '/projects/[id]' },
+      '/projects/fH0mB7CwtNyb6q7aMeFrH': { page: '/projects/[id]' },
+      '/projects/7qu1adrZHMfGnrnRQfdBx4': { page: '/projects/[id]' },
       '/history': { page: '/history' },
       '/photo': { page: '/photo' },
     }
